d0d06fa09524b53c46a0a71c72287450bfe6a285
Kate Sills <kate@agoric.com>
2020-10-14 14:31:05 -0700

    chore: add close and liquidate

--- packages/zoe/src/contracts/loan/close.js
+++ packages/zoe/src/contracts/loan/close.js
@@ -1,42 +1,81 @@
 // @ts-check
-import { assert } from '@agoric/assert';
-import { E } from '@agoric/eventual-send';
+import { assert, details } from '@agoric/assert';
 
-import { assertProposalShape, trade, natSafeMath } from '../../contractSupport';
- 
- // If you repay (amount borrowed + interest) you get all collateral back
- const close = repaySeat => {
-  assertProposalShape(repaySeat, {
-    give: { Loan: null },
-    want: { Collateral: null },
-  });
+import { assertProposalShape, trade } from '../../contractSupport';
 
-  const repaid = repaySeat.getAmountAllocated('Loan');
-  const required = loanMath.add(borrowedAmount, interestAmount);
+// The amount which must be repaid is just the amount loaned plus
+// interest (aka stability fee)
 
-  // You must pay off the entire remainder
-  assert(
-    loanMath.isGTE(repaid, required),
-    details`Not enough Loan assets have been repaid.  ${required} is required, but only ${repaid} was repaid.`,
-  );
+// QUESTION: how is interest (aka stability fee on Maker) calculated?
+// Maker calculates on every block?
 
-  // Cannot use `swap` or `swapExact` helper because the lender doesn't have a want
-  trade(
-    zcf,
-    {
-      seat: repaySeat,
-      gains: { Collateral: lenderSeat.getAmountAllocated('Collateral') },
-    },
-    {
-      seat: lenderSeat,
-      gains: { Loan: required },
-    },
-  );
-  repaySeat.exit();
-  lenderSeat.exit();
-  zcf.shutdown('your loan is closed, thank you for your business');
+export const makeCloseLoanInvitation = (
+  zcf,
+  collSeat,
+  lenderSeat,
+  getBorrowedAmount,
+  getInterest,
+) => {
+  // If you repay (amount borrowed + interest) you get all collateral
+  // back
+
+  // Also closes contract
+  const repayAndClose = repaySeat => {
+    assertProposalShape(repaySeat, {
+      give: { Loan: null },
+      want: { Collateral: null },
+    });
+
+    const loanMath = zcf.getTerms().maths.Loan;
+
+    const repaid = repaySeat.getAmountAllocated('Loan');
+    const borrowedAmount = getBorrowedAmount();
+    const interestAmount = getInterest();
+    const required = loanMath.add(borrowedAmount, interestAmount);
+
+    // You must pay off the entire remainder
+    assert(
+      loanMath.isGTE(repaid, required),
+      details`Not enough Loan assets have been repaid.  ${required} is required, but only ${repaid} was repaid.`,
+    );
+
+    // Cannot use `swap` or `swapExact` helper because the collSeat
+    // doesn't have a want.
+
+    // Transfer the collateral to the repaySeat and remove the
+    // required loan tokens.
+    trade(
+      zcf,
+      {
+        seat: repaySeat,
+        gains: { Collateral: collSeat.getAmountAllocated('Collateral') },
+      },
+      {
+        seat: collSeat,
+        gains: { Loan: required },
+      },
+    );
+
+    // Transfer the repaid loan tokens to the lender
+    trade(
+      zcf,
+      {
+        seat: lenderSeat,
+        gains: { Loan: required },
+      },
+      {
+        seat: collSeat,
+        gains: {},
+      },
+    );
+
+    repaySeat.exit();
+    lenderSeat.exit();
+    collSeat.exit();
+    zcf.shutdown('your loan is closed, thank you for your business');
+  };
+
+  // Note: we can't put the required repayment in the
+  // customProperties because it will change
+  return zcf.makeInvitation(repayAndClose, 'repayAndClose');
 };
-
-// Note: we can't put the required repayment in the
-// customProperties because it will change
-const makeRepayLoanInvitation = () => zcf.makeInvitation(repay, 'repay');
--- packages/zoe/src/contracts/loan/liquidate.js
+++ packages/zoe/src/contracts/loan/liquidate.js
@@ -1,5 +1,83 @@
-// triggered by priceOracle
+import { E } from '@agoric/eventual-send';
+// @ts-check
 
-export const liquidate = () => {
-  
-}
+import { depositToSeat, withdrawFromSeat, trade } from '../../contractSupport';
+
+// Question: does the borrower get anything back on liquidation? Or is
+// it only the lender?
+
+// triggered by priceOracle. Also closes contract
+/**
+ * 
+ * @param {ContractFacet} zcf 
+ * @param {ZCFSeat} lenderSeat 
+ * @param {ZCFSeat} collSeat 
+ * @param {() => Amount} getBorrowedAmount 
+ * @param {() => Amount} getInterest 
+ */
+export const makeLiquidate = (
+  zcf,
+  lenderSeat,
+  collSeat,
+  priceOracle,
+  autoswap,
+  getBorrowedAmount,
+  getInterest,
+) => {
+  const liquidate = () => {
+    const loanMath = zcf.getTerms().maths.Loan;
+    const collateralBrand = zcf.getTerms().brands.Collateral;
+    const zoeService = zcf.getZoeService();
+
+    const borrowedAmount = getBorrowedAmount();
+    const interestAmount = getInterest();
+    const required = loanMath.add(borrowedAmount, interestAmount);
+
+    // How much collateral do we have to sell to repay the required
+    // amount?
+    // TODO: add some buffer in case the price changes?
+    const collateralToSell = await E(priceOracle).getPriceOut(
+      required,
+      collateralBrand,
+    );
+
+    const { Collateral: collateralPayment } = await withdrawFromSeat(zcf, collSeat, {
+      Collateral: collateralToSell,
+    });
+
+    const proposal = harden({
+      want: { Out: required },
+      give: { In: collateralToSell },
+    });
+
+    const payments = harden({ In: collateralPayment });
+
+    const swapInvitation = E(autoswap).getSwapInvitation();
+    const autoswapSeat = E(zoeService).offer(
+      swapInvitation,
+      proposal,
+      payments,
+    );
+
+    const collateralPayout = await E(autoswapSeat).getPayout('In');
+    const loanPayout = await E(autoswapSeat).getPayout('Out');
+
+    const allocation = await E(autoswapSeat).getCurrentAllocation();
+
+    const amounts = harden({ Collateral: allocation.Out, Loan: allocation.In });
+
+    await depositToSeat(zcf, lenderSeat, amounts, { Collateral: collateralPayout, Loan: loanPayout });
+
+    // move anything left over on collSeat over to lenderSeat
+    trade(zcf, 
+      { seat: collSeat, gains: {} },
+      { seat: lenderSeat, gains: collSeat.getCurrentAllocation() },
+    );
+
+    lenderSeat.exit();
+    collSeat.exit();
+    zcf.shutdown('your loan had to be liquidated');
+  };
+
+  return liquidate;
+};
